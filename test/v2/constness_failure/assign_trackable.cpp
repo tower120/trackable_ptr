a7c1d83688897621f3809f4ab42c32f29e66b989
tower120 <tower120@gmail.com>
2019-05-15 19:27:07 +0300

    update

--- test/v2/constness_failure/assign_trackable.cpp
+++ test/v2/constness_failure/assign_trackable.cpp
@@ -10,6 +10,7 @@ using namespace tower120::v2;
 
 int main(){
     trackable<const test_struct> d1{1};
-    trackable_ptr<test_struct> p1 {&d1};
+    trackable_ptr<test_struct> p1;
+    p1 = &d1;
     return 0;
 }
--- test/v2/constness_failure/assign_trackable_base.cpp
+++ test/v2/constness_failure/assign_trackable_base.cpp
@@ -8,8 +8,14 @@
 
 using namespace tower120::v2;
 
+struct Data : trackable_base {
+    int i;
+    explicit Data(int i) : i(i){}
+};
+
 int main(){
-    trackable<const test_struct> d1{1};
-    trackable_ptr<test_struct> p1 {&d1};
+    const Data d1{1};
+    trackable_ptr<Data> p1;
+    p1 = &d1;
     return 0;
 }
