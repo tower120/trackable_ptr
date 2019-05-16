38c173965c932405442096a83768a72db0608b15
tower120 <tower120@gmail.com>
2019-05-16 19:44:02 +0300

    fix

--- test/constness_failure/assign_trackable.cpp
+++ test/constness_failure/assign_trackable.cpp
@@ -9,7 +9,7 @@
 using namespace tower120;
 
 int main(){
-    trackable<const test_struct> d1{1};
+    const trackable<test_struct> d1{1};
     trackable_ptr<test_struct> p1;
     p1 = &d1;
     return 0;
